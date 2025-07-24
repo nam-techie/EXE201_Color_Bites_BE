8d2369812c642b7dca3e3b6d0078293cb660c83b
NhatAn <annhnse180074@fpt.edu.vn>
2025-07-25 00:50:50 +0700

    setup codebase

---
+++ src/main/java/com/exe201/color_bites_be/ColorBitesBeApplication.java
@@ -0,0 +1,13 @@
+package com.exe201.color_bites_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorBitesBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColorBitesBeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/exe201/color_bites_be/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.exe201.color_bites_be.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    private static final String SECURITY_SCHEMES = "BearerAuth";
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Kakan API").version("1.0").description("API for Kakan"))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEMES))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes(SECURITY_SCHEMES, new SecurityScheme()
+                                .name(SECURITY_SCHEMES)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
