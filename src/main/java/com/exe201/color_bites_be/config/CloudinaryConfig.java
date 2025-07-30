09ebcf76cbf14883426a961e23cf1ad927c909f5
NhatAn <annhnse180074@fpt.edu.vn>
2025-07-30 20:50:47 +0700

    add file env

--- src/main/java/com/exe201/color_bites_be/config/CloudinaryConfig.java
+++ src/main/java/com/exe201/color_bites_be/config/CloudinaryConfig.java
@@ -1,6 +1,7 @@
 package com.exe201.color_bites_be.config;
 
 import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,13 +10,22 @@ import java.util.Map;
 
 @Configuration
 public class CloudinaryConfig {
-    @Bean
-    public Cloudinary cloudinary(){
-        Map<String, Object> config = new HashMap<>();
-        config.put("cloud_name", "dnpguhdhx");
-        config.put("api_key", "822112182435981");
-        config.put("api_secret", "I0RKszI-tJiM1KM5vMhfE7zVuTU");
-        return new Cloudinary(config);
 
+    @Value("${CLOUDINARY_CLOUD_NAME}")
+    private String cloudName;
+
+    @Value("${CLOUDINARY_API_KEY}")
+    private String apiKey;
+
+    @Value("${CLOUDINARY_API_SECRET}")
+    private String apiSecret;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        Map<String, Object> config = new HashMap<>();
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
+        return new Cloudinary(config);
     }
 }
