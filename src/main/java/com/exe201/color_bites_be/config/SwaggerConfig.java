259e1249968448e32158c265730449ef1adf0444
thanglh-hub <thanglhse180075@fpt.edu.vn>
2025-10-22 13:40:29 +0700

    feat: Add local development server configuration to Swagger on top

--- src/main/java/com/exe201/color_bites_be/config/SwaggerConfig.java
+++ src/main/java/com/exe201/color_bites_be/config/SwaggerConfig.java
@@ -20,6 +20,9 @@ public class SwaggerConfig {
                         .title("ColorBites API")
                         .version("1.0")
                         .description("API for ColorBites"))
+                        .addServersItem(new Server()
+                        .url("http://localhost:8080")
+                        .description("Local Development Server"))
                 // Thêm server URLs để Swagger UI sử dụng đúng HTTPS trên Railway
                 .addServersItem(new Server()
                         .url("https://api-mumii.namtechie.id.vn")
@@ -32,9 +35,6 @@ public class SwaggerConfig {
                 .addServersItem(new Server()
                         .url("https://mumii-be.namtechie.id.vn")
                         .description("Deployment BE - Azure"))
-                .addServersItem(new Server()
-                        .url("http://localhost:8080")
-                        .description("Local Development Server"))
                 .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEMES))
                 .components(new Components()
                         .addSecuritySchemes(SECURITY_SCHEMES,
